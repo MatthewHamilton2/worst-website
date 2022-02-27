81b13d2fbfed05f22803fd3b4c2515eb78a3c96c
MatthewHamilton2 <99368699+MatthewHamilton2@users.noreply.github.com>
2022-02-27 15:11:14 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -8,3 +8,18 @@ function showhide(id1, id2){
 function change(id1, id2){
 	document.getElementById(id1).src=(id2)
 }
+
+function readmore(id1, id2, id3) {
+  var dots = document.getElementById(id1);
+  var moreText = document.getElementById(id2);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    moreText.style.display = "none";
+	document.getElementById(id3).innerHTML = "Read more";
+  } else {
+    dots.style.display = "none";
+    moreText.style.display = "inline";
+	document.getElementById(id3).innerHTML = "Read less";
+  }
+}
